d460bccea647b988635e604f161d67c43cee0fab
VladTheJunior <vladthejunior@gmail.com>
2022-07-31 13:59:49 +0800

    v0.0.1

---
+++ js/discord-widget.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    var requestURL = 'https://discordapp.com/api/guilds/925683862099595335/widget.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function () {
+        var discord = request.response;
+        document.querySelector('.discord-invite-header-count').innerHTML = '<div class="discord-invite-header-online-status"></div>' + discord['presence_count'] + ' в сети';
+    }
+});
